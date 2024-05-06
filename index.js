07cc9a7b2a81b7b8439cf48f542c64a395b7f2a6
Suresh Packiry <sureshpackiry73@gmail.com>
2024-05-06 19:47:09 +1000

    watching a video and slowly implementing each step

---
+++ index.js
@@ -0,0 +1,18 @@
+// Create Node Class
+class listNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// Create list Class
+class linkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+  // Append
+  append(data) {
+    
+  }
+}
